b4c191075becd020f9cad9b24ec6b4fbfcfdc989
Rizvi-Mohammed <77290339+Rizvi-Mohammed@users.noreply.github.com>
2025-02-28 13:22:58 -0500

    data

---
+++ data/index.ts
@@ -0,0 +1,194 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize team collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Cloud and Infrastructure Experience",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building an AWS Cloud scripts library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "MeowMart",
+    des: "A full end-to-end Ecommerce bidding system to select your fav Cat, using Apache Tomcat, Java, NextJS and containerized with Docker",
+    img: "/p1.svg",
+    iconLists: ["/next.svg", "/Java.svg"],
+    link: "https://github.com/Rizvi-Mohammed/MeowMart",
+  },
+  {
+    id: 2,
+    title: "Yoom - Video Conferencing App",
+    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "",
+  }
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Abbas was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abbas's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Abbas is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Abbas was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abbas's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Abbas is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Abbas was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abbas's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Abbas is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Abbas was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abbas's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Abbas is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Abbas was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Abbas's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Abbas is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Lead Web Developer - York University",
+    desc: "Revamped psychology research website using modern frontend and backend flows - with a low-code final product.",
+    className: "md:col-span-2",
+    thumbnail: "/YU.jpg",
+  },
+  {
+    id: 2,
+    title: "Software Engineer - Royal Bank of Canada",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/rbc-4.svg",
+  },
+  {
+    id: 3,
+    title: "Web Developer - WebKraftz",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/wk.jpeg",
+  },
+  {
+    id: 4,
+    title: "Peer Mentor and TA - Lassonde School of Engineering",
+    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/lse.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/mohammed-abbas-rizvi-532204230/"
+  }
+];
