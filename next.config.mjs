c3744dfc468eea196cba2c4f25532613a1386184
Rizvi-Mohammed <77290339+Rizvi-Mohammed@users.noreply.github.com>
2025-03-06 16:49:04 -0500

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    trailingSlash: true
-};
+const nextConfig = {};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
